aeee33d90cb15a4fd13ddab68269c038a8b1b530
val-art01 <mayoujord@gmail.com>
2023-12-16 15:01:14 +0100

    add function to sort trains by date, departure station and arrival station

--- controllers/trianController.js
+++ controllers/trianController.js
@@ -31,8 +31,10 @@ export const createTrain = async (req, res) => {
 // Get all trains
 export const getAllTrains = async (req, res) => {
     try {
+        const {limit} = req.query;
         const trains = await Train.find();
-        res.status(200).json(trains);
+        const sortedTrains = sortTrains(trains, limit)
+        res.status(200).json(sortedTrains);
     } catch (err) {
         console.error('Error getting trains', err);
         res.status(500).json({ response: 'Internal server error' });
@@ -103,3 +105,27 @@ export const deleteTrain = async (req, res) => {
         res.status(500).json({ response: 'Internal server error' });
     }
 };
+
+// Function to sort trains by date, departure station and arrival station
+export const sortTrains =  (trains, limit = 10) =>{
+    return trains
+        .sort((a, b) =>{
+            //sort by date
+            const dateA = new Date(a.time_departure);
+            const dateB = new Date(a.time_departure);
+            if(dateA < dateB) return -1;
+            if(dateA > dateB) return 1;
+
+            // If dates are equal, sort by departure station
+            const startStationA = a.start_station.toLowerCase();
+            const startStationB = b.start_station.toLowerCase();
+            if(startStationA < startStationB) return -1;
+            if(startStationA > startStationB) return 1;
+
+            // If departure stations are equal, sort by arrival station
+            const endStationA = a.end_station.toLowerCase();
+            const endStationB = b.end_station.toLowerCase();
+            return endStationA.localeCompare(endStationB);
+        })
+        .slice(0, limit); //limit number trains
+}
