23984775dc8aaec41694a35325c9624411f75ce2
val-art01 <mayoujord@gmail.com>
2023-12-16 04:31:17 +0100

    field adjustment

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,25 +1,25 @@
 import express from 'express'
-import * as userController from './../controllers/userController.js';
+import {getAllUsers, getUserById, updateUser, setAdmin, setWorker, deleteUser} from './../controllers/userController.js'
 import { verifyTokenAndAdmin , verifyTokenAndAuthorization} from './../middleware/verify.js'
 
 const router = express.Router()
 
 // Get all users
-router.get('/', verifyTokenAndAdmin, userController.getAllUsers);
+router.get('/', verifyTokenAndAdmin, getAllUsers);
 
 // Get a specific user by ID
-router.get('/:id', verifyTokenAndAuthorization, userController.getUserById);
+router.get('/:id', verifyTokenAndAuthorization, getUserById);
 
 // Update a user
-router.put('/:id', verifyTokenAndAuthorization, userController.updateUser);
+router.put('/:id', verifyTokenAndAuthorization, updateUser);
 
 // Set or unset a user as admin
-router.put('/:id/admin', userController.setAdmin);
+router.put('/:id/admin', verifyTokenAndAdmin, setAdmin);
 
 // Set or unset a user as worker
-router.put('/:id/worker', userController.setWorker);
+router.put('/:id/user', setWorker);
 
 // Delete a user by id
-router.delete('/:id', verifyTokenAndAuthorization, userController.deleteUser);
+router.delete('/:id', verifyTokenAndAuthorization, deleteUser);
 
 export default router
