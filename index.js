f52567618720d56a8c0fcb242c49dd54c89251a0
val-art01 <mayoujord@gmail.com>
2023-12-16 04:41:23 +0100

    Stop the application if the database connection fails.

--- index.js
+++ index.js
@@ -4,32 +4,35 @@ import mongoose from 'mongoose'
 import { config } from 'dotenv'
 import swaggerUi from 'swagger-ui-express'
 import swaggerDocument from './swagger.json' assert { type: 'json' }
-import trainRoute from './routes/trainRoute.js'
+// import trainRoute from './routes/trainRoute.js'
 import userRoute from './routes/userRoute.js'
 import authRoute from './routes/authRoute.js'
-import trainStationRouter from './routes/trainStationRouter.js';
+// import trainStationRouter from './routes/trainStationRouter.js';
 
 
 const app = express()
-const PORT = 8080
+const PORT = process.env.PORT
 config()
 
-mongoose.connect(`mongodb://${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`
-).then(() => {
-    console.log('Connected to the database')
-}).catch((error) => {
-    console.error('Error connecting to the database: ', error)
-})
+mongoose.connect(`mongodb://${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`)
+  .then(() => {
+    console.log('Connected to the database');
+  })
+  .catch((error) => {
+    console.error('Error connecting to the database: ', error);
+    process.exit(1);
+});
+
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}))
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
 
 app.use(cors());
-app.use('/api/train', trainRoute);
+// app.use('/api/train', trainRoute);
 app.use('/api/users', userRoute);
 app.use('/api/auth', authRoute);
-app.use('/api/trainstations', trainStationRouter)
+// app.use('/api/trainstations', trainStationRouter)
 
 app.use((req, res) => {
     res.status(404).json({ message: `API not found at ${req.url}` })
