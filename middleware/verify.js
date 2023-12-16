3e48b4c96d6b95775e66f7f74213ba78e0018af6
val-art01 <mayoujord@gmail.com>
2023-12-16 04:37:24 +0100

    ok

--- middleware/verify.js
+++ middleware/verify.js
@@ -1,9 +1,7 @@
 import jwt from 'jsonwebtoken'
-import mongoose from 'mongoose'
 
 const verifyToken = (req, res, next) => {
     const authHeader = req.headers.authorization
-
     if (authHeader) {
         const token = authHeader.split(" ")[1]
         
@@ -19,7 +17,7 @@ const verifyToken = (req, res, next) => {
 
 const verifyTokenAndAdmin = (req, res, next) => {
     verifyToken(req, res, () => {
-        if (req.user.role=="admin") {
+        if (req.user.role==="admin") {
             next()
         } else {
             res.status(403).json("You are not allowed to do that!") 
@@ -30,53 +28,33 @@ const verifyTokenAndAdmin = (req, res, next) => {
 const verifyTokenAndAuthorization = (req, res, next) => {
     verifyToken(req, res , () =>{
         try {
-
-        // Vérifier si l'utilisateur est un admin
-        const isAdmin = verifyAdmin
-
-        // Vérifier si l'utilisateur est un employé
-        const isEmployee = verifyWorker
-
-        // Vérifier si l'utilisateur a le même ID que celui spécifié dans la route
-        const isSameUserId = req.params.id === req.user.id
-
-        // Vérifier si l'utilisateur peut accéder aux informations d'un autre utilisateur
-        const canAccessUserInfo = isAdmin || isEmployee || isSameUserId;
-
-        // Vérifier les différentes contraintes
-        if (req.method === 'GET' && !canAccessUserInfo) {
-            res.status(403).json({ error: "Permission denied" });
-        } else if (req.method === 'PUT' && !(isSameUserId || isAdmin)) {
-            res.status(403).json({ error: "Permission denied" });
-        } else if (req.method === 'DELETE' && !isSameUserId) {
-            res.status(403).json({ error: "Permission denied" });
-        } else {
-            next(); // Autoriser l'accès à la route
+            // Check if the user is an admin
+            const isAdmin = req.user.role === 'admin';
+            // Check if the user is an employee
+            const isEmployee = req.user.role === 'customer';
+            // Check if the user has the same ID as the one specified in the route
+            const isSameUserId = req.params.id === req.user.id;
+            // Check if the user can access another user's information
+            const canAccessUserInfo = (isAdmin || isEmployee) && isSameUserId;
+            // Check various constraints
+            if (req.method === 'GET' && !canAccessUserInfo) {
+                res.status(403).json({ error: "Permission denied" });
+            } else if (req.method === 'PUT' && !(isSameUserId || isAdmin)) {
+                res.status(403).json({ error: "Permission denied" });
+            } else if (req.method === 'DELETE' && !isSameUserId) {
+                res.status(403).json({ error: "Permission denied" });
+            } else {
+                next(); // Autoriser l'accès à la route
+            }
+        } catch (error) {
+            console.error(error);
+            res.status(500).json({ error: "Internal server error" });
         }
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ error: "Internal server error" });
-    }
-})
-}
-
-const verifyAdmin  = (req, res,) => {
-        if (req.user.role=="admin") {
-            res.true
-        } else {
-            res.False
-        }
-}
-
-const verifyWorker  = (req, res,) => {
-    if (req.user.role=="worker") {
-        res.true
-    } else {
-        res.False
-    }
+    })
 }
 
 export {
+    verifyToken,
     verifyTokenAndAdmin,
     verifyTokenAndAuthorization
 }
