dd4a722f9bb2fd2f1ea8b30f6daf135795987cf7
val-art01 <mayoujord@gmail.com>
2023-12-16 04:33:20 +0100

    field adjustment

--- models/User.js
+++ models/User.js
@@ -2,10 +2,11 @@ import mongoose from "mongoose"
 
 const UserSchema = new mongoose.Schema(
     {
-        username: { type: String, required: true, unique: true },
+        pseudo: { type: String, required: true },
+        email: { type: String, required: true, unique: true },
         password: { type: String, required: true },
         iv: { type: String, required: true },
-        role: { type: String, default: "custommer" }
+        role: { type: String, default: "customer" } 
     },
     { timestamps: true }
 )
