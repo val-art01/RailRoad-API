a90a12f7538cf7148cbef5f23c4afdfab9361897
val-art01 <mayoujord@gmail.com>
2023-12-17 09:38:33 +0100

    model TrainStation

--- models/Train.js
+++ models/Train.js
@@ -2,9 +2,11 @@ import mongoose from "mongoose"
 
 const TrainSchema = new mongoose.Schema(
     {
-        name: { type: String, required: true, unique: true },
+        name: { type: String, required: true },
         start_station: { type: String, required: true, unique: true },
         end_station: { type: String, required: true },
+        // start_station: { type: mongoose.Schema.Types.ObjectId, ref: 'TrainStation', required: true },
+        // end_station: { type: mongoose.Schema.Types.ObjectId, ref: 'TrainStation', required: true },        
         time_departure: { type: String, required: true },
     },
     { timestamps: true }
--- models/TrainStation.js
+++ models/TrainStation.js
@@ -2,13 +2,14 @@ import mongoose from "mongoose"
 
 const TrainStationSchema = new mongoose.Schema(
     {
-        name: { type: String, required: true, unique: true },
-        open_hour: { type: String, required: true, unique: true },
+        name: { type: String, required: true},
+        open_hour: { type: String, required: true },
         close_hour: { type: String, required: true },
         img: { type: String, required: true },
+        // img: { data: Buffer, contentType: String },
         trains: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Train' }]
     },
     { timestamps: true }
 )
 
-export default mongoose.model('User', TrainStationSchema)
+export default mongoose.model('TrainStation', TrainStationSchema)
